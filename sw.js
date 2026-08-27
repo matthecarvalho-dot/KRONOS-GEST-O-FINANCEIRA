a4383fce40118c73c6e48363e5793a0f38ed7a12
Kronos.system <matthecarvalho@gmail.com>
2026-08-27 13:51:17 -0300

    Implement caching for index.html in service worker
    Added service worker install event to cache index.html.

--- sw.js
+++ sw.js
@@ -1,4 +1,17 @@
-self.addEventListener('fetch', (event) => {
-  // Apenas intercepta requisições para permitir o funcionamento como App
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('kronos-v1').then((cache) => {
+      return cache.addAll([
+        './index.html'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });
